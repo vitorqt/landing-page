a63186e44a31b6bce63650b5dd8458551ee1e8cd
Vitor Rodrigues <vitorqt@hotmail.com>
2018-08-13 20:46:59 -0300

    Adiciona pacote MDB

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdbvue/build/css/mdb.css';
+
 Vue.config.productionTip = false
 
 new Vue({
